325326c1d9a2b1540b4af71552e66eb7be4c107f
Admen-UltraSoft <admenn94@gmail.com>
2022-07-25 17:38:19 +0200

    react basics

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Card from "./components/Card";
+
+const Main = () => {
+    return (
+        <>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </>
+    )
+}
+
+export default Main
---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = () => {
+    var text = "This is a card!!"
+
+
+    const func = () => {
+        return "nekoj string"
+    }
+
+    const broj = 5
+
+    const niza = [<p>Hi</p>, <p>There</p>]
+
+
+    return <div>
+        <h1>{text}</h1>
+    </div>
+}
+
+export default Card
